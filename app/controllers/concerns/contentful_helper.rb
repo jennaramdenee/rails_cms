62a24ac1a95084834905c04565b480e9486e35b2
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2018-02-23 16:23:19 +0000

    Add helper for Contentful unpublish

--- app/controllers/concerns/contentful_helper.rb
+++ app/controllers/concerns/contentful_helper.rb
@@ -1,10 +1,25 @@
-# module ContentfulHelper
-#
-#   def contentful_client_setup
-#     client = Contentful::Client.new(
-#       access_token: 'b8574398e6e5f3763e9e363f7d59813de4810f2419e9e7238284adcc76ec8505',
-#       space: '3t6txrpcx29n'
-#     )
-#   end
-#
-# end
+require 'contentful/management'
+
+module ContentfulHelper
+  def self.unpublish_entry(request_body)
+    ContentfulHelper.set_up(request_body)
+    entry = @client.entries.find(@space_id, @entry_id) if @space_id && @entry_id
+    entry.unpublish if entry
+  end
+
+  private
+
+  def self.set_up(request_body)
+    @client = Contentful::Management::Client.new(ENV['CONTENTFUL_ACCESS_TOKEN'])
+    @space_id = ContentfulHelper.space_id(request_body)
+    @entry_id = ContentfulHelper.entry_id(request_body)
+  end
+
+  def self.entry_id(request_body)
+    request_body.dig('sys', 'id')
+  end
+
+  def self.space_id(request_body)
+    request_body.dig('sys', 'space', 'sys', 'id')
+  end
+end
